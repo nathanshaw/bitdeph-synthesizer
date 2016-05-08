a8259562adf1fb7947c40fc3d2346303c1536a44
nathanshaw <nathanshawsemail@gmail.com>
2016-05-08 11:32:02 -0700

    added class for random numbers

---
+++ BiTDEPH Synthesizer/Random.cpp
@@ -0,0 +1,9 @@
+//
+//  Random.cpp
+//  BiTDEPH Synthesizer
+//
+//  Created by Nathan Villicaña-Shaw on 5/8/16.
+//  Copyright © 2016 Nathan Villicaña-Shaw. All rights reserved.
+//
+
+#include "Random.hpp"
---
+++ BiTDEPH Synthesizer/Random.h
@@ -0,0 +1,14 @@
+//
+//  Random.hpp
+//  BiTDEPH Synthesizer
+//
+//  Created by Nathan Villicaña-Shaw on 5/8/16.
+//  Copyright © 2016 Nathan Villicaña-Shaw. All rights reserved.
+//
+
+#ifndef Random_hpp
+#define Random_hpp
+
+#include <stdio.h>
+
+#endif /* Random_hpp */
